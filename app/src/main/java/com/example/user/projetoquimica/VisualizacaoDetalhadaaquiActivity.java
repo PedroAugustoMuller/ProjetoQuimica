a76116041d4d6b2422f849e9eab81bcf6b246511
leliss213 <leandroferreiramine@gmail.com>
2022-09-13 13:03:35 -0300

    Alteração na tela de detalhamento
    Alteração dos edittext para textview.

--- app/src/main/java/com/example/user/projetoquimica/VisualizacaoDetalhadaaquiActivity.java
+++ app/src/main/java/com/example/user/projetoquimica/VisualizacaoDetalhadaaquiActivity.java
@@ -5,11 +5,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.user.classesDominio.Pergunta;
 
 public class VisualizacaoDetalhadaaquiActivity extends AppCompatActivity {
-    EditText etVisualizacaoDetalhadaEnunciado, etVisualizacaoDetalhadaOpcaoA, etVisualizacaoDetalhadaOpcaoB, etVisualizacaoDetalhadaOpcaoC, etVisualizacaoDetalhadaOpcaoD, etVisualizacaoDetalhadaOpcaoE, etVisualizacaoDetalhadaRespostaEscolhida, etVisualizacaoDetalhadaRespostaCerta;
+    TextView tvVisualizacaoDetalhadaEnunciado, tvVisualizacaoDetalhadaOpcaoA, tvVisualizacaoDetalhadaOpcaoB,
+            tvVisualizacaoDetalhadaOpcaoC, tvVisualizacaoDetalhadaOpcaoD, tvVisualizacaoDetalhadaOpcaoE,
+            tvVisualizacaoDetalhadaRespostaEscolhida, tvVisualizacaoDetalhadaRespostaCerta;
     Pergunta pergunta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,26 +20,26 @@ public class VisualizacaoDetalhadaaquiActivity extends AppCompatActivity {
         setContentView(R.layout.activity_visualizacao_detalhadaaqui);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        etVisualizacaoDetalhadaEnunciado = findViewById(R.id.etVisualizacaoDetalhadaEnunciado);
-        etVisualizacaoDetalhadaOpcaoA = findViewById(R.id.etVisualizacaoDetalhadaOpcaoA);
-        etVisualizacaoDetalhadaOpcaoB = findViewById(R.id.etVisualizacaoDetalhadaOpcaoB);
-        etVisualizacaoDetalhadaOpcaoC = findViewById(R.id.etVisualizacaoDetalhadaOpcaoC);
-        etVisualizacaoDetalhadaOpcaoD = findViewById(R.id.etVisualizacaoDetalhadaOpcaoD);
-        etVisualizacaoDetalhadaOpcaoE = findViewById(R.id.etVisualizacaoDetalhadaOpcaoE);
-        etVisualizacaoDetalhadaRespostaCerta = findViewById(R.id.etVisualizacaoDetalhadaRespostaCerta);
-        etVisualizacaoDetalhadaRespostaEscolhida = findViewById(R.id.etVisualizacaoDetalhadaRespostaEscolhida);
+        tvVisualizacaoDetalhadaEnunciado = findViewById(R.id.tvVisualizacaoDetalhadaEnunciado);
+        tvVisualizacaoDetalhadaOpcaoA = findViewById(R.id.tvVisualizacaoDetalhadaOpcaoA);
+        tvVisualizacaoDetalhadaOpcaoB = findViewById(R.id.tvVisualizacaoDetalhadaOpcaoB);
+        tvVisualizacaoDetalhadaOpcaoC = findViewById(R.id.tvVisualizacaoDetalhadaOpcaoC);
+        tvVisualizacaoDetalhadaOpcaoD = findViewById(R.id.tvVisualizacaoDetalhadaOpcaoD);
+        tvVisualizacaoDetalhadaOpcaoE = findViewById(R.id.tvVisualizacaoDetalhadaOpcaoE);
+        tvVisualizacaoDetalhadaRespostaCerta = findViewById(R.id.tvVisualizacaoDetalhadaRespostaCerta);
+        tvVisualizacaoDetalhadaRespostaEscolhida = findViewById(R.id.tvVisualizacaoDetalhadaRespostaEscolhida);
 
         Intent it = getIntent();
         if(it != null && it.hasExtra("pergunta")){
             pergunta = (Pergunta) getIntent().getSerializableExtra("pergunta");
-            etVisualizacaoDetalhadaEnunciado.setText(pergunta.getEnunciado());
-            etVisualizacaoDetalhadaOpcaoA.setText(pergunta.getOpcaoA());
-            etVisualizacaoDetalhadaOpcaoB.setText(pergunta.getOpcaoB());
-            etVisualizacaoDetalhadaOpcaoC.setText(pergunta.getOpcaoC());
-            etVisualizacaoDetalhadaOpcaoD.setText(pergunta.getOpcaoD());
-            etVisualizacaoDetalhadaOpcaoE.setText(pergunta.getOpcaoE());
-            etVisualizacaoDetalhadaRespostaEscolhida.setText(Character.toString(pergunta.getOpcaoEscolhida()));
-            etVisualizacaoDetalhadaRespostaCerta.setText(Character.toString(pergunta.getAlternativaCorreta()));
+            tvVisualizacaoDetalhadaEnunciado.setText(pergunta.getEnunciado());
+            tvVisualizacaoDetalhadaOpcaoA.setText(pergunta.getOpcaoA());
+            tvVisualizacaoDetalhadaOpcaoB.setText(pergunta.getOpcaoB());
+            tvVisualizacaoDetalhadaOpcaoC.setText(pergunta.getOpcaoC());
+            tvVisualizacaoDetalhadaOpcaoD.setText(pergunta.getOpcaoD());
+            tvVisualizacaoDetalhadaOpcaoE.setText(pergunta.getOpcaoE());
+            tvVisualizacaoDetalhadaRespostaEscolhida.setText(Character.toString(pergunta.getOpcaoEscolhida()));
+            tvVisualizacaoDetalhadaRespostaCerta.setText(Character.toString(pergunta.getAlternativaCorreta()));
         }
 
     }

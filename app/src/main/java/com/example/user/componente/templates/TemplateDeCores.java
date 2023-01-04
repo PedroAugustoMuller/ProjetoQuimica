5d2d4fd734ec917cb30a3d746d20b8f3823f572b
PedroAugustoMuller <pedro.muller.a@gmail.com>
2023-01-04 14:04:25 -0300

    TemplateDeCores
    Paleta de cores dos níveis do Quiz

---
+++ app/src/main/java/com/example/user/componente/templates/TemplateDeCores.java
@@ -0,0 +1,19 @@
+package com.example.user.componente.templates;
+
+import android.graphics.Color;
+
+public class TemplateDeCores {
+
+    public static final int[] PALETA_NIVEIS ={
+            //COBRE//
+            Color.rgb(132,86,37),
+            //BRONZE
+            Color.rgb(212,170, 84),
+            //PRATA
+            Color.rgb(166,166,166),
+            //OURO
+            Color.rgb(253,206,41),
+            //DIAMANTE
+            Color.rgb(56,182,255)
+    };
+}

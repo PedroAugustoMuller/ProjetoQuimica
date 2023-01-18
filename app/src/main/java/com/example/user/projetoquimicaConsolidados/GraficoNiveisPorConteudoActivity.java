29baaaba3f189938d21639704820f913ffe87b13
PedroAugustoMuller <pedro.muller.a@gmail.com>
2023-01-18 14:19:28 -0300

    ClasseIntermediariaGraficos
    Implementação da ClasseIntermediariaGraficos - DezQuestionariosActivity e NiveisPorConteudoActivity

--- app/src/main/java/com/example/user/projetoquimicaConsolidados/GraficoNiveisPorConteudoActivity.java
+++ app/src/main/java/com/example/user/projetoquimicaConsolidados/GraficoNiveisPorConteudoActivity.java
@@ -1,6 +1,5 @@
 package com.example.user.projetoquimicaConsolidados;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -8,15 +7,14 @@ import android.support.v7.widget.Toolbar;
 import com.example.user.banco.ConteudoDB;
 import com.example.user.banco.InformacoesApp;
 import com.example.user.banco.NivelConteudoDB;
+import com.example.user.classesDominio.ClasseIntermediariaGraficos;
 import com.example.user.classesDominio.NivelConteudo;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
-import com.github.mikephil.charting.utils.ColorTemplate;
+
 
 import java.util.ArrayList;
 
@@ -26,6 +24,7 @@ public class GraficoNiveisPorConteudoActivity extends AppCompatActivity {
     ConteudoDB conteudoDB;
     NivelConteudoDB nivelConteudoDB;
     InformacoesApp informacoesApp;
+    ClasseIntermediariaGraficos classeIntermediariaGraficos;
     ArrayList<NivelConteudo> listaNiveisConteudos = new ArrayList<>();
     String[] label;
     String[] yLabel = {"","Cobre","Bronze","Prata","Ouro","Diamante"};
@@ -46,26 +45,33 @@ public class GraficoNiveisPorConteudoActivity extends AppCompatActivity {
         nivelConteudoDB = new NivelConteudoDB(this);
         //InformacoesApp
         informacoesApp = (InformacoesApp) getApplicationContext();
+        //ClasseIntermediariaGraficos
+        classeIntermediariaGraficos = new ClasseIntermediariaGraficos(this);
 
-        listaNiveisConteudos = nivelConteudoDB.buscaConteudosComNivel(conteudoDB.buscaConteudos(informacoesApp.getTipoConteudo()), informacoesApp.getMeuUsuario());
-        label = new String[listaNiveisConteudos.size()];
 
         ConfiguraGrafico();
     }
 
     public void ConfiguraGrafico(){
-        ArrayList<BarEntry> listaEntradas = new ArrayList<>();
+//        ArrayList<BarEntry> listaEntradas = new ArrayList<>();
+//        for (int x = 0; x < listaNiveisConteudos.size(); x++){
+//            NivelConteudo meuNivelConteudo = listaNiveisConteudos.get(x);
+//            label[x] = listaNiveisConteudos.get(x).getConteudo().getNomeConteudo();
+//            listaEntradas.add(new BarEntry(x , (float)meuNivelConteudo.getNivel().getValor(), meuNivelConteudo.getImagemNivel(this)));
+//        }
+//
+//        BarDataSet setNiveisPorConteudo = new BarDataSet(listaEntradas, "Níveis por Conteúdo");
+//        setNiveisPorConteudo.setDrawValues(false);
+//        setNiveisPorConteudo.setColors(ColorTemplate.JOYFUL_COLORS);
+//        BarData data = new BarData(setNiveisPorConteudo);
+//        data.setDrawValues(false);
+        listaNiveisConteudos = nivelConteudoDB.buscaConteudosComNivel(conteudoDB.buscaConteudos(informacoesApp.getTipoConteudo()), informacoesApp.getMeuUsuario());
+        label = new String[listaNiveisConteudos.size()];
         for (int x = 0; x < listaNiveisConteudos.size(); x++){
-            NivelConteudo meuNivelConteudo = listaNiveisConteudos.get(x);
             label[x] = listaNiveisConteudos.get(x).getConteudo().getNomeConteudo();
-            listaEntradas.add(new BarEntry(x , (float)meuNivelConteudo.getNivel().getValor(), meuNivelConteudo.getImagemNivel(this)));
         }
 
-        BarDataSet setNiveisPorConteudo = new BarDataSet(listaEntradas, "Níveis por Conteúdo");
-        setNiveisPorConteudo.setDrawValues(false);
-        setNiveisPorConteudo.setColors(ColorTemplate.JOYFUL_COLORS);
-        BarData data = new BarData(setNiveisPorConteudo);
-        data.setDrawValues(false);
+        BarData data = classeIntermediariaGraficos.configuraGraficoNiveisPorConteudo(listaNiveisConteudos);
         bcGraficoNiveisPorConteudo.setData(data);
         bcGraficoNiveisPorConteudo.animateY(2000);
         bcGraficoNiveisPorConteudo.getDescription().setEnabled(false);
@@ -74,6 +80,7 @@ public class GraficoNiveisPorConteudoActivity extends AppCompatActivity {
         bcGraficoNiveisPorConteudo.getLegend().setEnabled(false);
         bcGraficoNiveisPorConteudo.setExtraBottomOffset(10f);
         bcGraficoNiveisPorConteudo.isDragEnabled();
+        bcGraficoNiveisPorConteudo.setVisibleXRangeMaximum(5f);
         //YAxis
         XAxis xAxis = bcGraficoNiveisPorConteudo.getXAxis();
         YAxis left = bcGraficoNiveisPorConteudo.getAxisLeft();
@@ -93,8 +100,7 @@ public class GraficoNiveisPorConteudoActivity extends AppCompatActivity {
         xAxis.setDrawGridLines(false);
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
         xAxis.setValueFormatter(new IndexAxisValueFormatter(label));
-        //xAxis.setDrawLabels();
-        System.out.println("Terminei");
+        
     }
 
 
